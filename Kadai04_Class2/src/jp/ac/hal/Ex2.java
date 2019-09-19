c27dc385d945a9a18aee38bda8ce7e57d2c14f21
masato <ponsima1140@gmail.com>
2019-09-19 12:06:40 +0900

    [ Kadai04 ] step2 age

---
+++ Kadai04_Class2/src/jp/ac/hal/Ex2.java
@@ -0,0 +1,18 @@
+package jp.ac.hal;
+
+public class Ex2 {
+//	private int age;
+
+//	public Ex2(int age) {
+//		this.age = age;
+//	}
+
+	public void judge(int age) {
+		if(age < 20) {
+			System.out.println("未成年");
+		}else {
+			System.out.println("成年");
+		}
+	}
+
+}
