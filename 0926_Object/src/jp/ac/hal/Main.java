0943c90f48c2fd316e3f5289fb655c1455556cca
masato <ponsima1140@gmail.com>
2019-09-26 13:00:19 +0900

    [new] Object型

---
+++ 0926_Object/src/jp/ac/hal/Main.java
@@ -0,0 +1,14 @@
+package jp.ac.hal;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Object o;
+		o = new Object();
+		o = 1;
+		o = "";
+		o = 1.1;
+		o = new StringBuilder();
+	}
+
+}
